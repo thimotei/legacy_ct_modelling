3ec11a3bd1c26a898f5957451896242de965dd79
thimotei <timothywilliamrussell@gmail.com>
2022-02-15 10:22:43 +0000

    cleaning empirical data script

---
+++ R/process_data.R
@@ -0,0 +1,55 @@
+process_data <- function(data_raw) {
+  
+  data_out <- data_raw[, swab_date := dmy(swab_date)] %>% 
+    .[barcode %like% "49U", swab_date := swab_date - 1] %>% 
+    .[symptom_onset_date == "unknown", symptom_onset_date := NA] %>% 
+    .[, symptom_onset_date := dmy(symptom_onset_date)] %>% 
+    .[, date_dose_1 := dmy(date_dose_1)] %>% 
+    .[, date_dose_2 := dmy(date_dose_2)] %>% 
+    .[, date_dose_3 := dmy(date_dose_3)] %>% 
+    .[ct != "unknown"] %>% 
+    .[, ct := as.numeric(ct)] %>% 
+    .[result == "Negative", ct := 40] %>% 
+    .[swab_type == "VTM" & result == "Negative", ct_adjusted := 40] %>%
+    .[swab_type == "VTM" & result != "Negative", 
+      ct_adjusted := adjustment.fun(adjustment.dt[param == "alpha", me],
+                                    adjustment.dt[param == "beta", me], ct)] %>%
+    .[swab_type != "VTM", ct_adjusted := ct]
+  
+  #--- removing duplicate VTM vs Dry swabs
+  barcodes.to.remove <-  data_out[swab_type == "Dry" | swab_type == "VTM"] %>% 
+    .[, total_obs_by_date := .N, by = c("ID", "swab_date")] %>% 
+    .[total_obs_by_date > 1, c("ID", "swab_date", "barcode", "swab_type")] %>% 
+    .[order(ID, swab_date)] %>% 
+    .[swab_type == "Dry", barcode]
+  
+  data_out_no_dups <- data_out[!barcode %in% barcodes.to.remove] 
+  
+  #--- conditioning on only tests performed in the Crick, leaving out
+  #--- UCLH swabs for now
+  data_out_no_dups <- data_out_no_dups[centre == "crick"]
+  
+  #--- counting number of positive swabs by individual
+  no_pos_cts <- data_out_no_dups[(result == "Positive" |
+                                  result == "Inconclusive") & 
+                                  is.na(ct_adjusted) == FALSE] %>% 
+    .[, .N, by = c("ID", "infection_ID")]
+  
+  data_out_no_dups <- merge.data.table(data_out_no_dups,
+                                       no_pos_cts, 
+                                       by = c("ID", "infection_ID")) %>%  
+    setnames(., 
+             c("N", "number_vaccines (14 days pre ix)"), 
+             c("no_pos_results", "no_vaccines"))
+  
+  data_out_no_dups[result == "Inconclusive"]
+  
+  setnames(data_out_no_dups,
+           c("ID", "infection_ID", "ct", "ct_adjusted"),
+           c("id", "infection_id", "ct_unadjusted", "ct_value"))
+  
+  #--- conditioning out observations with NA Ct values (how can we use these?)
+  data_out_no_dups <- data_out_no_dups[is.na(ct_unadjusted) == FALSE]
+    
+  return(data_out_no_dups)
+}
