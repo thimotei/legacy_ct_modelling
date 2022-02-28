64a058a4c72056b961b7d8b6cf15d9efcd36d8cb
seabbs <s.e.abbott12@gmail.com>
2022-02-28 11:46:42 +0000

    update to plot populaton cts with generalised tools

--- R/plot.R
+++ R/plot.R
@@ -51,8 +51,58 @@ plot_obs_ct <- function(ct_dt, ct_traj, pp, traj_alpha = 0.02, onsets = TRUE,
     custom_plot_theme() +
     theme(legend.position = "bottom") +
     labs(
-      colour = "Swab type", x = "Days since first positive test", 
+      colour = "Swab type", x = "Days since first positive test",
       y = "CT value"
     )
+  return(plot)
+}
+
+plot_ct_pp <- function(pp, sum_pp, onsets = TRUE, clod = 40, alpha = 0.05,
+                       ...) {
+
+  plot <- ggplot(pp) +
+    aes(x = t, y = ct_value, group = interaction(.iteration, .chain), ...) +
+    scale_colour_brewer(palette = "Dark2")
+
+  if (!is.null(pp$onset_time) & onsets) {
+    plot <- plot +
+      geom_vline(aes(xintercept = onset_time), linetype = 2, alpha = 0.8)
+  }
+
+  if (!is.null(clod)) {
+    plot <- plot +
+      geom_hline(yintercept = clod, linetype = 3, alpha = 0.8)
+  }
+
+   if (!missing(sum_pp)) {
+     plot <- plot +
+      geom_ribbon(
+        data = sum_pp,
+        aes(ymin = lo90, ymax = hi90, y = NULL, group = NULL), alpha = 0.2
+      ) +
+      geom_ribbon(
+        data = sum_pp,
+        aes(ymin = lo60, ymax = hi60, y = NULL, group = NULL), alpha = 0.2
+      )
+   }
+
+  plot <- plot +
+    geom_line(alpha = alpha)
+
+   plot <- plot +
+    custom_plot_theme() +
+    labs(
+      x = "Days since infection",
+      y = "CT value"
+    )
+  return(plot)
+}
+
+plot_density <- function(draws, ...) {
+  plot <- ggplot(draws) +
+    geom_density(aes(x = value, ...), alpha = 0.2) +
+    facet_wrap(~variable, nrow = 2, scales = "free") +
+    labs(x = "", y = "Probability density")
+
   return(plot)
 }
