b5723154fb138c3aa9fdf098351369f79a004141
seabbs <s.e.abbott12@gmail.com>
2022-02-28 16:18:53 +0000

    minor regor

--- scripts/inference.R
+++ scripts/inference.R
@@ -3,7 +3,6 @@ library(data.table)
 library(ggplot2)
 library(truncnorm)
 library(cmdstanr)
-library(cowplot)
 library(stringr)
 library(purrr)
 library(lubridate)
@@ -14,52 +13,28 @@ library(tidyr)
 files <- list.files("R", "*.R", full.names = TRUE)
 walk(files, source)
 
-dt_raw <- fread("data/ct_values_clean.csv")
+# load in data processed in scripts/process-data.R
+dt_raw <- fread("data/processed-data.csv")
 
-# processing data, adding machine readable dates, moving dates
-# back for certain swabs, based on data collection advice and adjusting
-# all wet swabs upwards according to the fitted dry vs wet adjustment
-# factor
-dt_clean <- process_data(dt_raw)
-
-#--- TEMPORARY, for now, as "invalid" and "inconclusive" both have many
-#--- observations consistent with positive Ct values, keeping them for now
-#--- will discuss with Crick partners about this
-dt_clean[result == "Invalid", result := "Positive"]
-dt_clean[result == "Inconclusive", result := "Positive"]
-dt_clean[result == "Negative", pcr_res := 0]
-dt_clean[result == "Positive", pcr_res := 1]
-
-#--- Drop patients with gaps between positive tests of more than 60 days
-ids_spurious_gaps <- dt_clean[,
-  .(spurious = any(abs(time_since_first_pos) > 60) || any(abs(onset_time) > 60)
-   ), by = "id"
-]
-ids_spurious_gaps[spurious == TRUE][]
-dt_clean <- dt_clean[ids_spurious_gaps, on = "id"]
-dt_clean <- dt_clean[spurious == FALSE | is.na(spurious)]
-
-#--- Drop subject with ID 978 due to large mismatch between onset and positive
-#--  tests
-dt_clean <- dt_clean[id != 978]
-
-#--- quick plots of the raw data, stratified by variant
+# quick plots of the raw data, stratified by variant
 dt_2_tests <- subset_data(
   dt_clean, voc = c("Delta", "Omicron"), no_pos_swabs = 2
 )
-dt_delta <- subset_data(dt_clean, voc = c("Delta"), no_pos_swabs = 2)
-dt_omicron <- subset_data(dt_clean, voc = "Omicron", no_pos_swabs = 2)
-
 p1_raw <- plot_obs_ct(dt_2_tests)
 
-#--- compile models
+# Specify the CT model design matrix
+ct_model <- subject_design(~ 1 + VOC, data = dt_2_tests, params = "all")
+
+# Translate data and model specification to stan format
+stan_data <- data_to_stan(
+  dt_2_tests, ct_model = ct_model,
+  likelihood = TRUE, onsets = TRUE
+)
+
+# compile model
 mod <- cmdstan_model("stan/ct_trajectory_model.stan", include_paths = "stan")
 
-#--- fit
-stan_data <- data_to_stan(dt_2_tests, likelihood = TRUE, onsets = TRUE)
-stan_data_delta <- data_to_stan(dt_delta, likelihood = TRUE, onsets = TRUE)
-stan_data_omicron <- data_to_stan(dt_omicron, likelihood = TRUE, onsets = TRUE)
-
+# fit
 fit <- mod$sample(
   data = stan_data,
   init = stan_inits(stan_data),
@@ -69,26 +44,7 @@ fit <- mod$sample(
   iter_sampling = 1000
 )
 
-fit_delta <- mod$sample(
-  data = stan_data_delta,
-  init = stan_inits(stan_data_delta),
-  chains = 4,
-  parallel_chains = 4,
-  iter_warmup = 1000,
-  iter_sampling = 1000
-)
-
-
-fit_omicron <- mod$sample(
-  data = stan_data_omicron,
-  init = stan_inits(stan_data_omicron),
-  chains = 4,
-  parallel_chains = 4,
-  iter_warmup = 1000,
-  iter_sampling = 1000
-)
-
-#--- extract and plot posterior predictions
+# extract and plot posterior predictions
 ct_draws <- extract_ct_trajectories(fit)
 
 ct_summary <- summarise_draws(
@@ -110,7 +66,7 @@ pp_plot <- plot_obs_ct(
 
 ggsave("outputs/figures/pp.png", pp_plot, height = 16, width = 16)
 
-#--- extract and plot population level posterior predictions
+# extract and plot population level posterior predictions for the CT model
 draws <- extract_draws(fit)
 
 pop_pp <- plot_ct_summary(
