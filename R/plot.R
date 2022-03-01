84a483f42f19f79e31f8e68d8fec2df4f0d625af
seabbs <s.e.abbott12@gmail.com>
2022-03-01 10:01:37 +0000

    use ! statements for false

--- R/plot.R
+++ R/plot.R
@@ -1,20 +1,21 @@
 custom_plot_theme <- function(flip = FALSE, legend_arg = FALSE) {
-  
-  custom_plot_theme = list(
+
+  custom_plot_theme <- list(
     theme_bw(11),
     theme(strip.placement = "outside"),
     geom_vline(aes(xintercept = -Inf)),
-    guides(color = guide_legend(override.aes = list(fill = NA))))
-  
-  if(flip == FALSE) { 
+    guides(color = guide_legend(override.aes = list(fill = NA)))
+  )
+
+  if (!flip) {
     append(custom_plot_theme, geom_hline(aes(yintercept = -Inf)))
   }
-  
-  if(legend_arg == FALSE) {
+
+  if (!legend_arg) {
     append(custom_plot_theme,
            theme(legend.title = element_blank()))
   }
-  
+
   return(custom_plot_theme)
 }
 
