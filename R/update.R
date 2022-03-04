91880f98740fa7bc0b6733b755e002fb3c5e9131
seabbs <s.e.abbott12@gmail.com>
2022-03-04 14:45:55 +0000

    add incubation period labels

--- R/update.R
+++ R/update.R
@@ -2,7 +2,8 @@ update_ct_variables <- function(draws, reverse = FALSE) {
 
 draws <- data.table::copy(draws)
 params <- c(
-  "c_0", "c_p", "c_s","t_p", "t_s", "t_lod", "inc_mean", "inc_sd"
+  "c_0", "c_p", "c_s","t_p", "t_s", "t_lod", "inc_mean", "inc_sd", 
+  "emp_inc_mean", "emp_inc_sd"
 )
 
 clean_params <- c(
@@ -12,7 +13,9 @@ clean_params <- c(
   "Time of peak",
   "Time of switch",
   "Time of limit of detection",
-  "Incubation period (mean)",
+  "Incubation period (log mean)",
+  "Incubation period (log sd)",
+    "Incubation period (mean)",
   "Incubation period (sd)"
 )
 
