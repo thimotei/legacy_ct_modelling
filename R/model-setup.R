1b069b32f6a84d1bec6c7325befed2f39fbbdbc3
seabbs <s.e.abbott12@gmail.com>
2022-03-16 14:59:53 +0000

    check using simulation

--- R/model-setup.R
+++ R/model-setup.R
@@ -143,9 +143,11 @@ stan_inits <- function(dt) {
       }
     }
 
-    if (dt$ct_preds > 0 & dt$adj_ct) {
-      inits$beta_ct_shift <- rnorm(dt$ct_preds, 0, 0.01)
-      inits$beta_ct_scale <- rnorm(dt$ct_preds, 0, 0.01)
+    if (dt$ct_preds > 0) {
+      if (dt$adj_ct > 0) {
+        inits$beta_ct_shift <- rnorm(dt$ct_preds, 0, 0.01)
+        inits$beta_ct_scale <- rnorm(dt$ct_preds, 0, 0.01)
+      }
     }
 
     if (dt$any_onsets == 1) {
